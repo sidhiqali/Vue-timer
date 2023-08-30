f1332bfc855b2e0568090a2b283ff180c3ee2d2d
sidhiqali <sidhiqsidhiqali@gmail.com>
2023-08-30 14:49:56 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <h1>My Reaction Timer</h1>
+  <button
+    @click="start"
+    :disabled="isPlaying"
+  >
+    Play
+  </button>
+  <Block
+    v-if="isPlaying"
+    :delay="delay"
+  />
+</template>
+
+<script>
+import Block from "./components/block.vue";
+export default {
+  name: "App",
+  components: {
+    Block,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      console.log(this.delay);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  margin-top: 70px;
+  font-family: "Times New Roman", Times, serif;
+  font-weight: 300;
+  color: darkgreen;
+}
+button {
+  width: 70px;
+  height: 30px;
+  background-color: rgb(19, 121, 200);
+  border: 1px solid green;
+  color: white;
+}
+button:disabled {
+ opacity: 0.4;
+}
+</style>
---
+++ src/components/block.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    v-if="showModel"
+    class="block"
+  >
+    <div>Click Me</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Block",
+  props: ["delay"],
+  data() {
+    return {
+      showModel: false,
+    };
+  },
+  mounted() {
+    console.log("mounted");
+    setTimeout(() => {
+      this.showModel = true;
+      console.log('done')
+    }, this.delay);
+  },
+  updated(){
+    console.log("updated");
+  }
+};
+</script>
+
+<style>
+.block {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 30px;
+  width: 350px;
+  height: 200px;
+  background-color: rgb(12, 123, 101);
+  color: white;
+  border-radius: 20px;
+}
+</style>
